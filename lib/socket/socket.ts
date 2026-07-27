656b79e9e59af7aa7a9388706082645afb8d0d48
ad <adildesai1707@gmail.com>
2026-07-27 23:27:33 +0530

    Frontend ready for deployment

--- lib/socket/socket.ts
+++ lib/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://192.168.2.28:5000" , {
+export const socket = io("https://e-commerce-backend-tz02.onrender.com" , {
 // export const socket = io("http://localhost:5000" , {
   withCredentials: true,
   autoConnect: false,
