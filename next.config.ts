a63d9a238a15a324949f3992c46b36f202c4f689
ad <adildesai1707@gmail.com>
2026-07-28 01:02:34 +0530

    Configure static export for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,32 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   allowedDevOrigins: ["192.168.2.28" , "localhost"],
+//   // allowedDevOrigins: ["localhost"],
+
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.2.28" , "localhost"],
-  // allowedDevOrigins: ["localhost"],
+  output: "export",
 
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
