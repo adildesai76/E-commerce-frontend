029fbb1e7f8ddf43f2c5eeff3ca40298577f5e15
ad <adildesai1707@gmail.com>
2026-07-28 01:08:37 +0530

    Configure Next.js for Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,10 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   allowedDevOrigins: ["192.168.2.28" , "localhost"],
-//   // allowedDevOrigins: ["localhost"],
-
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  allowedDevOrigins: ["192.168.2.28" , "localhost"],
+  // allowedDevOrigins: ["localhost"],
 
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
