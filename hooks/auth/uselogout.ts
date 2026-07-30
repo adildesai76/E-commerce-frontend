aa8cf30f325394a758d73f04542ef0b13a6459e2
ad <adildesai1707@gmail.com>
2026-07-30 22:50:13 +0530

    Configure Next.js for Netlify

--- hooks/auth/uselogout.ts
+++ hooks/auth/uselogout.ts
@@ -38,6 +38,7 @@ export const useLogout = () => {
       // Remove frontend cookie first
       document.cookie =
         "auth_token=; Max-Age=0; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      localStorage.clear();
 
       // Then call backend logout
       return await logoutapi();
